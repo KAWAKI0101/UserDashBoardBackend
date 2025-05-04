114d3d1fc0198d84157d5fe679615f5fbf3f8156
KAWAKI0101 <hv3346433@gmail.com>
2025-05-05 01:08:31 +0530

    Updated server.js to use environment variables for Mongo URI

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 const express = require("express");
-const mongoose =  require("mongoose");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
-//Load env
+// Load environment variables from .env file
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-//Import 
+// Import routes
 const userRoutes = require('./routes/user.routes.js');
 app.use('/api/users', userRoutes);
 
+// MongoDB connection using mongoose
+const mongoURI = process.env.MONGO_URI; // Fetch the Mongo URI from environment variables
 
-// Db connection
-const connectDb = require('./config/Db');
-connectDb();
-
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch((err) => console.log('Database connection failed:', err));
 
 // Start Server
-const PORT =  process.env.PORT || 10000;
-app.listen(PORT,() => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
